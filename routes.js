73912f1fab4906aa0a3a5143931de9ecf1c396d1
FranklinSaad <franklinpinheirosaad@hotmail.com>
2020-10-04 21:19:17 -0300

    Fourth Commit

--- routes.js
+++ routes.js
@@ -1,3 +1,4 @@
+const { Router } = require('express')
 const express = require('express')
 const routes = express.Router()
 const teachers = require('./teachers')
@@ -13,6 +14,10 @@ routes.get('/teachers',  function(req, res) {
 routes.get('/teachers/create',  function(req, res) {
     return res.render("teachers/create")
 })
+
+routes.get('/teachers/:id', teachers.show)
+
+routes.get('/teachers/:id/edit', teachers.edit)
 
 routes.post("/teachers", teachers.post)
 
--- teachers.js
+++ teachers.js
@@ -1,5 +1,25 @@
 const fs = require('fs')
 const data = require('./data.json')
+const { age, date } = require('./utils')
+
+exports.show = function(req, res) {
+    const { id } = req.params
+
+    const foundTeacher = data.teachers.find(function (teacher) {
+        return id == teacher.id
+    })
+
+    if (!foundTeacher) return res.send('Professor não encontrado!')
+
+    const teacher = {
+        ...foundTeacher,
+        age: age(foundTeacher.birth),
+        materias: foundTeacher.materias.split(','),
+        created_at: new Intl.DateTimeFormat("en-US").format(foundTeacher.created_at),
+    }
+
+    return res.render("teachers/show", { teacher })
+}   
 
 exports.post = function (req, res) {
     const keys = Object.keys(req.body)
@@ -20,10 +40,10 @@ exports.post = function (req, res) {
         avatar_url,
         name,
         birth,
-        created_at,
         escolaridade,
         type,
         materias,
+        created_at,
     }) 
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
@@ -32,4 +52,23 @@ exports.post = function (req, res) {
         return res.redirect("teachers")
     })
 
+}
+
+exports.edit = function(req, res) {
+    const { id } = req.params
+
+    const foundTeacher = data.teachers.find(function (teacher) {
+        return id == teacher.id
+    })
+
+    if (!foundTeacher) return res.send('teacher not found!')
+
+    const teacher = {
+        ...foundTeacher,
+        birth: date(foundTeacher.birth)
+    }
+
+    date(foundTeacher.birth)
+
+    return res.render('teachers/edit', { teacher })
 }
